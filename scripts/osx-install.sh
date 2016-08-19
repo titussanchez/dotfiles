486de4bb18213fd0acd122c79fd6d178235e59bc
Titus Sanchez <titusjo@gmail.com>
2016-08-18 23:57:51 -0400

    Update git submodules before installing

--- scripts/osx-install.sh
+++ scripts/osx-install.sh
@@ -2,9 +2,12 @@
 set -e
 
 pushd ~/dotfiles
+git submodule update --init --recursive
+
 stow -vv mjolnir
 stow -vv slate
 stow -vv tmux
 stow -vv vim --ignore=.vim
-test !-e ~/.vim && cp -a vim/.vim ~/
+test ! -e ~/.vim && cp -a vim/.vim ~/
+
 popd
--- scripts/ubuntu-install.sh
+++ scripts/ubuntu-install.sh
@@ -2,8 +2,11 @@
 set -e
 
 pushd ~/dotfiles
+git submodule update --init --recursive
+
 stow -vv i3
 stow -vv config
 stow -vv vim --ignore=.vim
 test ! -e ~/.vim && cp -a vim/.vim ~/
+
 popd
